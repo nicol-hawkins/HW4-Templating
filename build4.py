918cc05f6752dc6baa9af7f2b14973b0eae30393
Nicol <nicolhawkins@yahoo.com>
2019-11-22 17:34:17 -0800

    Homework 4: up to phase 3

--- build4.py
+++ build4.py
@@ -1,69 +1,40 @@
-def main():
-    for page in pages:
-        file_name = page["filename"]
-        output = page["output"]
-        title = page["title"]
-
-        # Read in the content which
-        # is located is now stored in file_name
-        content = open(file_name).read()
-        template = page_builder(content, title)
-        
-        # write page by opening the output(docs)
-        # by accessing it within the dictionary.
-        # write it to template cause templates
-        # contains the function page_builder,
-        # which actually constructs the pages.
-        open(page["output"], "w+").write(template)
-       
-
-# This function constructs the webpage by replacing
-# empty braces with actual content and titles within
-# the newly constructed base template.
-def page_builder(content, title):
-    template = open("templates/base.html").read()
-    template_plus_content = template.replace("{{content}}", content)
-    finished_page = template_plus_content.replace("{{title}}", title)
-    return finished_page
-
-
-import glob
-all_html_files = glob.glob("content/*.html")
-print(all_html_files)
-
 import os
-file_path = "content/.html"
-file_name = os.path.basename(file_path)
-print(file_name)
-name_only, extension = os.path.splitext(file_name)
-print(name_only)
-
-for file_path in glob.glob("content/*.html"):
-    print(file_path)
-
-
+import glob
+from jinja2 import Template
 
 pages = []
-pages.append({
-    "filename": "content/index.html",
-    "title": "Index",
-    "output": "docs/index.html"
-    })
 
-pages.append({
-    "filename": "content/about.html",
-    "title": "About",
-    "output": "docs/about.html"
-    })
+def create_pages_list():
+    all_html_files = glob.glob('content/*.html')
 
-pages.append({
-    "filename": "content/resume.html",
-    "title": "Resume",
-    "output": "docs/resume.html"
-    })
+    for content_file_path in all_html_files:
+        file_path = content_file_path
+        file_name = os.path.basename(file_path)
+        name_only, extension = os.path.splitext(file_name)
+        name_only_title = name_only.title()
+        output_file = os.path.join('docs', file_name)
 
-print(pages)
-   
+        pages.append({
+                "file_path": file_path,
+                "title": name_only,
+                "output": output_file,
+                "file_name": file_name,
+            })
+        
+
+def main():
+    create_pages_list()
+    content_of_template = open('templates/base.html', 'r').read()
+    template = Template(content_of_template)
+
+    for page in pages:
+        content = open(page['file_path'], 'r').read()
+        html_full_result = template.render (
+                            title = page['title'],
+                            content = content,
+                        )
+        open(page['output'], 'w+').write(html_full_result)
+
 if __name__ == "__main__":
     main()
 
