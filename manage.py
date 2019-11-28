4b428d1ead19a226f7d08bcd5de65b310543ced7
Nicol <nicolchawkins@gmail.com>
2019-11-27 18:26:12 -0800

    Finished writing manage.py new command

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
-import utils.py
 import sys
+import utils
+
 
 print("This is argv:", sys.argv)
 command = sys.argv[1]
@@ -8,7 +9,11 @@ if command == "build":
     create_pages_list()
     main()
 elif command == "new":
-    # Need help with what goes here
+    new_content_page = '''
+            <h1> New Content! <h1>
+            <p> new content... </p>
+        '''
+    open("content/new_content_page.html", "w+").write(new_content_page)
 elif command == None:
     print('''Usage:
                 Rebuild site: python manage.py build
